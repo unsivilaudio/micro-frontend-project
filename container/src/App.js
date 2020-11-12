b48866657a764f9b6b110ef3c8b0727437a6a523
unsivil <unsivilaudio@gmail.com>
2020-11-12 14:58:29 -0700

    initial commit, scaffold out container and marketing

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MarketingApp from './Components/MarketingApp';
+
+const App = props => {
+    return (
+        <div className='Container'>
+            <h1>Micro-Frontends with React</h1>
+            <MarketingApp />
+        </div>
+    );
+};
+
+export default App;
