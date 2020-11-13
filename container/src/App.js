9f3f1853c09b882add592ce3ecf9fcf923e70485
unsivil <unsivilaudio@gmail.com>
2020-11-13 12:49:23 -0700

    typo fix

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './Components/MarketingApp';
 const App = props => {
     return (
         <div className='Container'>
-            <h1>Micro-Frontends with React</h1>
+            <h1>Micro-Frontends with React!</h1>
             <MarketingApp />
         </div>
     );
