44679bcc11e3d05182cca49166dfcde1757bb5a8
unsivil <unsivilaudio@gmail.com>
2020-11-13 14:15:28 -0700

    resolve class collisions

--- container/src/App.js
+++ container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './Components/MarketingApp';
 import Header from './Components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 const App = props => {
     return (
-        <BrowserRouter>
-            <div className='Container'>
-                <Header />
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div className='Container'>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
 
